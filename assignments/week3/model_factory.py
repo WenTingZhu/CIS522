ed706e089c566661acebc1119cd430100302fd76
wzhu <wzhu@alarm.com>
2023-02-07 12:25:10 -0500

    week3 hw bug

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,6 +15,4 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    return MLP(
-        input_dim, 4096, output_dim, 1, torch.nn.ReLU(), torch.nn.init.xavier_uniform_
-    )
+    return MLP(input_dim, 32, output_dim, 1, torch.nn.ReLU(), torch.nn.init.ones_)
