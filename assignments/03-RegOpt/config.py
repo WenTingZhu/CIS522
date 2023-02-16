48a18562b04faebed7190dd7fdcdff9d9e2f2acf
wzhu <wzhu@alarm.com>
2023-02-16 15:04:08 -0500

    HW3

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -2,29 +2,52 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, Normalize, ToTensor
+from torchvision.transforms import (
+    Compose,
+    Normalize,
+    ToTensor,
+    RandomHorizontalFlip,
+    RandomVerticalFlip,
+    RandomCrop,
+)
 
 
 class CONFIG:
+    """_summary_"""
 
-    batch_size = 64
-    num_epochs = 2
-    initial_learning_rate = 0.001
+    batch_size = 32
+    num_epochs = 15
+    initial_learning_rate = 0.0011
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-    }
+        # 'lr_lambda': [lambda epoch: 0.999],
+        # 'start_epoch': 20,
+        # 'step_size': 100,
+        # "gamma": 0.9,
+        # "milestones": [3000, 3600, 4000, 4800, 5400, 6000],
+        "T_0": 32,
+        "eta_min": 0.00035,
+        "T_mult": 32,
+    }  # gamma=0.9, milestones=[30,80]
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
-    )
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
+        amsgrad=True,
+    )  # lr=0.001, betas=(0.9, 0.999), weight_decay=0, amsgrad=False
 
     transforms = Compose(
         [
             ToTensor(),
+            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            RandomHorizontalFlip(),
+            RandomVerticalFlip(),
+            RandomCrop(32, padding=4),
         ]
     )
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,23 @@
 from typing import List
-
 from torch.optim.lr_scheduler import _LRScheduler
+import torch
+import math
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    def __init__(
+        self,
+        optimizer: torch.optim.Optimizer,
+        milestones: List = None,
+        step_size: int = None,
+        gamma: int = None,
+        start_epoch: int = None,
+        T_0: int = None,
+        T_mult: int = None,
+        eta_min: int = None,
+        lr_lambda: list = None,
+        last_epoch: int = -1,
+    ) -> None:
         """
         Create a new scheduler.
 
@@ -12,13 +25,76 @@ class CustomLRScheduler(_LRScheduler):
         if you need to add new parameters.
 
         """
-        # ... Your Code Here ...
+
+        if lr_lambda is not None:
+            self.lr_lambdas = lr_lambda
+        if step_size is not None:
+            self.step_size = step_size
+        if gamma is not None:
+            self.gamma = gamma
+        if start_epoch is not None:
+            self.start_epoch = start_epoch
+        if milestones is not None:
+            self.milestones = milestones
+        if T_0 is not None:
+            self.T_0 = T_0
+            self.T_i = T_0
+        if T_mult is not None:
+            self.T_mult = T_mult
+        if eta_min is not None:
+            self.eta_min = eta_min
+
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
+        """
+        Return learning rate
+
+        Returns:
+            List[float]: learning rate
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # return [i for i in self.base_lrs]
+        # LambdaLR
+        # return [base_lr * lmbda(self.last_epoch)
+        #         for lmbda, base_lr in zip(self.lr_lambdas, self.base_lrs)]
+        # MultiplicativeLR
+        # if self.last_epoch > self.start_epoch:
+        #     return [group['lr'] * lmbda(self.last_epoch)
+        #             for lmbda, group in zip(self.lr_lambdas, self.optimizer.param_groups)]
+        # else:
+        #     return [group['lr'] for group in self.optimizer.param_groups]
+        # StepLR
+        # if (self.last_epoch == 0) or (self.last_epoch % self.step_size != 0):
+        #     return [group['lr'] for group in self.optimizer.param_groups]
+        # return [group['lr'] * self.gamma
+        #         for group in self.optimizer.param_groups]
+        # MultiStepLR
+        # if self.last_epoch not in self.milestones:
+        #     return [group["lr"] for group in self.optimizer.param_groups]
+        # return [group["lr"] * self.gamma for group in self.optimizer.param_groups]
+        # Cosine
+        self.T_i *= self.T_mult ** (self.last_epoch // self.T_i)
+        return [
+            self.eta_min
+            + (base_lr - self.eta_min)
+            * (1 + math.cos(math.pi * (self.last_epoch % self.T_i) / self.T_i))
+            / 2
+            for base_lr in self.base_lrs
+        ]
+
+    def _get_closed_form_lr(self) -> List[float]:
+        """
+        Return closed form solution for learning rate
+
+        Returns:
+            List[float]: learning rate
+        """
+        return [
+            base_lr * self.gamma ** (self.last_epoch // self.step_size)
+            for base_lr in self.base_lrs
+        ]
