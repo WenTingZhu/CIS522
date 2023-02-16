8ce378b2d9acfd166e49399978747e9ec557fca4
wzhu <wzhu@alarm.com>
2023-02-16 16:15:11 -0500

    HW3

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -11,7 +11,7 @@ class CONFIG:
     batch_size = 64
     num_epochs = 10
     initial_learning_rate = 0.0011
-    initial_weight_decay = 0
+    initial_weight_decay = 1e-5
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
