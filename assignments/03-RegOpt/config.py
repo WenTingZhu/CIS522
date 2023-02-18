529d172069d3350c9ac07a630893e08199d56b35
wzhu <wzhu@alarm.com>
2023-02-18 14:48:48 -0500

    HW3

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -46,6 +46,6 @@ class CONFIG:
             # RandomHorizontalFlip(),
             # RandomCrop(32, padding=4),
             ToTensor(),
-            # Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]
     )
