6f02a67aae43cfcff7307d9c8324a81e9b961453
wzhu <wzhu@alarm.com>
2023-03-15 14:04:44 -0400

    HW4

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,62 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
+    """_summary_
+
+    Args:
+        torch (_type_): _description_
+    """
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        """_summary_
+
+        Args:
+            num_channels (int): _description_
+            num_classes (int): _description_
+        """
+        super(Model, self).__init__()
+
+        # self.conv1 = nn.Conv2d(num_channels, 8, kernel_size=3, stride=1, padding=1)
+        # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.ker = 5
+        self.pad = (self.ker - 1) // 2
+        self.nchan = 16
+        self.conv2 = nn.Conv2d(
+            num_channels, self.nchan, kernel_size=self.ker, stride=2, padding=self.pad
+        )
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(self.nchan * 8 * 8, num_classes)
+        # self.fc2 = nn.Linear(256, 100)
+        # self.fc3 = nn.Linear(100, num_classes)
+        nn.init.xavier_uniform_(self.conv2.weight)
+        nn.init.xavier_uniform_(self.fc1.weight)
+        # nn.init.xavier_uniform_(self.fc2.weight)
+        # nn.init.xavier_uniform_(self.fc3.weight)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """_summary_
+
+        Args:
+            x (torch.Tensor): _description_
+
+        Returns:
+            torch.Tensor: _description_
+        """
+        # x = self.conv1(x)
+        # x = F.relu(x)
+        # x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+
+        x = x.view(-1, self.nchan * 8 * 8)
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        x = self.fc1(x)
+
+        return x
